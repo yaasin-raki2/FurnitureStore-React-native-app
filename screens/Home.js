bf1f463e0cefb15e688656308e1c7b5ec9341e4c
Yassine <yassin23raki@gmail.com>
2020-12-04 02:21:24 +0100

    basic setup

---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text, SafeAreaView, StatusBar } from 'react-native';
+
+import { images, icons, COLORS, FONTS, SIZES } from '../constants/';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
