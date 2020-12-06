e03e1515f564e6e0191e63bb38f7498f79642175
Yassine <yassin23raki@gmail.com>
2020-12-06 11:39:30 +0100

    finishing the itemCardInfo

--- screens/ItemDetail.js
+++ screens/ItemDetail.js
@@ -75,6 +75,61 @@ const ItemDetail = ({ route, navigation }) => {
           style={{ width: '100%', height: '100%' }}
         >
           {renderHeader()}
+
+          {/* Product Tag */}
+
+          <View
+            style={{
+              position: 'absolute',
+              top: '45%',
+              left: '15%',
+              backgroundColor: COLORS.transparentLightGray1,
+              height: 40,
+              width: 40,
+              borderRadius: 80,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <View
+              style={{
+                borderRadius: 20,
+                backgroundColor: COLORS.blue,
+                height: 10,
+                width: 10,
+              }}
+            ></View>
+          </View>
+          <View
+            style={{
+              position: 'absolute',
+              top: '43%',
+              left: '30%',
+              flexDirection: 'row',
+              padding: SIZES.radius * 1.5,
+              backgroundColor: COLORS.transparentLightGray1,
+              width: '50%',
+              borderRadius: 10,
+            }}
+          >
+            <View style={{ flex: 2 }}>
+              <Text style={{ color: COLORS.darkGray }}>
+                {itemInfo.productName}
+              </Text>
+            </View>
+
+            <View
+              style={{
+                flex: 1.5,
+                alignItems: 'flex-end',
+                justifyContent: 'flex-end',
+              }}
+            >
+              <Text style={{ color: COLORS.darkGreen, ...FONTS.h3 }}>
+                $ {itemInfo.price.toFixed(2)}
+              </Text>
+            </View>
+          </View>
         </ImageBackground>
       );
     } else {
